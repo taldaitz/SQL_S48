27db0ffb1f6dc3a760d7fb8ec12615847ac6ce13
taldaitz <48563569+taldaitz@users.noreply.github.com>
2023-11-28 15:05:41 +0100

    Update billings.sql

--- billings.sql
+++ billings.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `bill`
 --
 
+CREATE DATABASE billings;
+
+USE billings;
+
 DROP TABLE IF EXISTS `bill`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
